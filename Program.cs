ac40af8ed174e9afc50520c863ecfecb3c15e1ef
silassousa <silassousadejesus@gmail.com>
2023-05-02 00:04:21 -0300

    refactor(): corrgindo conexões

--- Program.cs
+++ Program.cs
@@ -31,7 +31,7 @@ namespace marlin_challenge_back_end
           
             app.UseCors(options =>
             {
-                options.WithOrigins("https://marlin-challenge.vercel.app").AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin();
+                options.WithOrigins("https://marlin-challenge-front-end.vercel.app").AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin();
             });
 
             // Configure the HTTP request pipeline.
